d4d3ed4aa1733e66aa21ab0f171a65508633584c
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-11-08 03:57:11 +0530

    Nearly done everything except contact and services

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home";
+
+
+import "./styles/App.scss"
+import "./styles/header.scss"
+import "./styles/footer.scss"
+import "./styles/home.scss"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path= "/" element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "../styles/home.scss";
+import vg from "../assets/4.jpg";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Techmoon</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="FGraphics" />
+        <div>
+          <p>
+            The rich and interactive experiences we have come to expect on
+            mobile apps have created new standards and expectations for all
+            digital media including the web. The result is websites are evolving
+            to become more app-like in their rich functionality.
+          </p>
+        </div>
+      </div>
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            placeat quisquam quidem odit eligendi sequi error eos, sit iure,
+            quis mollitia beatae! Qui molestias nisi voluptas? Eum voluptate
+            voluptatum modi autem perspiciatis illo fugiat, quam repudiandae
+            accusantium atque ullam laudantium, sint excepturi quis voluptatibus
+            id esse porro tempore maxime exercitationem officia dicta reiciendis
+            dignissimos quos. Cumque reiciendis autem perferendis dolore! Minus
+            voluptatem adipisci unde voluptatibus, autem deserunt accusamus a,
+            ab asperiores magnam ad vitae, sed non quidem expedita alias magni
+            repudiandae atque laboriosam harum maiores aut! Praesentium sequi
+            nam quasi obcaecati harum illo aliquam porro beatae aspernatur odit.
+          </p>
+        </div>
+      </div>
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div style={{ animationDelay: "0.3s" }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div style={{ animationDelay: "0.5s" }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div style={{ animationDelay: "0.7s" }}>
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+            <div style={{ animationDelay: "0.9s" }}>
+              <AiFillYoutube />
+              <p>You Tube</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
